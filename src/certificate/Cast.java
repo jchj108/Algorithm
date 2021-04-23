2934ce5d097c439f402c8721183161f3f19ff2fe
jchj108 <jchj108@naver.com>
2021-04-24 02:04:16 +0900

    Cast

---
+++ src/certificate/Cast.java
@@ -0,0 +1,20 @@
+package certificate;
+
+public class Cast {
+
+	public static void main(String[] args) {
+		System.out.println("3/2 = " +3/2);
+		System.out.println("2/2 = "+2/2);
+		System.out.println("1/2 = "+1/2);
+		
+		System.out.println();
+
+		System.out.println("3%2 = " + 3%2);
+		System.out.println("2%2 = " + 2%2);
+		System.out.println("1%2 = " + 1%2);
+		
+		//³ª´°¼À¿¡¼­´Â  ³»¸².
+	
+	}
+
+}
